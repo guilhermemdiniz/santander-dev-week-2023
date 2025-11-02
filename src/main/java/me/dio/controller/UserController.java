84d10752fc70723c0c64a44b8f04c0281d593d42
Guilherme <guidiniz2019.18@gmail.com>
2025-11-02 16:03:46 -0300

    Primeira tentativa de publicação com Railway

---
+++ src/main/java/me/dio/controller/UserController.java
@@ -0,0 +1,37 @@
+package me.dio.controller;
+
+import me.dio.domain.model.User;
+import me.dio.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        var created = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+}
